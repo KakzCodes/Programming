b5cbaf05e3c5dbcdb1f54e0fe859e75edf581f2a
Yanick Kakou <kakzcodes@outlook.com>
2026-07-16 23:12:00 +0100

    space and time complexity

---
+++ DSA/time and space complexity.py
@@ -0,0 +1,15 @@
+"""
+
+When running a program, it requires time and space (memory utilisation) to execute.
+The time and space complexity of an algorithm is a measure of the amount of time and
+space required by an algorithm to run as a function of the length of the input.
+
+code can be executed on diffrent machines with different configurations,
+so the time and space complexity of an algorithm is expressed in terms of
+the size of the input rather than the actual time taken or memory used.
+
+time and space complexity is based on the size of the input, and it is expressed using Big O notation.
+
+the time is proportional to the space used.
+
+"""
